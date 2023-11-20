e43e5f98b9cb8a230a37c8a4784122a603199fa3
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-19 22:34:26 -0700

    added tailwind classes to logo and navlinks

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -12,10 +12,10 @@
 </script>
 
 <!--Primary Nav Links decoration-->
-<header class="bg-violet-600 text-white text-xl flex justify-center p-4">
-  <div>logo</div>
+<header class="bg-violet-600 text-white text-xl flex justify-center w-[auto] h-[100px] p-4">
+  <div><img src="images/common-ground-dance-logo.png" class="block w-[100px] h-[75px] p-4 mb-5 bg-cover" alt="Common Ground Dance logo"></div>
   <nav>
-    <ul class="flex justify-around gap-4">
+    <ul class="lg:flex text-2xl space-x-12 gap-4 p-4">
       {#each navLinks as {name, href}}
         <div class="hover:text-cyan-200 active:text-emerald-500">
           <li><a {href}>{name}</a></li>
