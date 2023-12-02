fa546556fb2bde5f7b5a4cdc507b424bfac52ac5
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-12-01 22:43:43 -0700

    made the carousel slide

---
+++ src/lib/components/ImageCarousel.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  type ImageType = {
+    title: string
+    src: string
+  }
+  export let images: ImageType[]
+</script>
+
+<ul class="flex overflow-x-auto gap-6 snap-x snap-mandatory before:shrink-0 before:w-[30vw] after:shrink-0 after:w-[30vw] p-4">
+  {#each images as {title,src}}
+    <li class="shrink-0 snap-center">
+      <img {title} {src} alt={title} class="w-[300px] h-[500px]"/>
+    </li>
+  {/each}
+</ul>
