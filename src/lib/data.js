7391a249061da80fd0d4f3baae36aa0b38b763b3
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-12-03 15:03:23 -0700

    edited the img line in object array for cards

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,32 +1,32 @@
 //dance program array
 export const danceObj = [
-  {img: 'images/programs/jazz-program-pexels.jpg',
+  {img: '/images/programs/jazz-program-pexels.jpg',
   title: 'Jazz',
   cta: 'Learn More',
   url: ''},
 
-  {img: 'images/programs/contemporary-dance--program-pexels.jpg',
-  title: 'Contemporary Dance',
+  {img: '/images/programs/contemporary-dance-program-pexels.jpg',
+  title: 'Contemporary',
   cta: 'Learn More',
   url: ''},
 
-  {img: 'images/programs/hip-hop-program-pexels.jpg',
+  {img: '/images/programs/hip-hop-program-pexels.jpg',
   title: 'Hip Hop',
   cta: 'Learn More',
   url: ''},
 
-  {img: 'images/programs/west-african-program-pexels.jpg',
+  {img: '/images/programs/west-african-program-pexels.jpg',
   title: 'West African',
   cta: 'Learn More',
   url: ''},
 
-  {img: 'images/programs/youth-classes-pexels.jpg',
+  {img: '/images/programs/youth-classes-pexels.jpg',
   title: 'Youth Classes',
   cta: 'Learn More',
   url: ''},
 
-  {img: 'images/programs/adult-classes-pexels.jpg',
+  {img: '/images/programs/adult-classes-pexels.jpg',
   title: 'Adult Classes',
   cta: 'Learn More',
   url: ''}
-]
+];
