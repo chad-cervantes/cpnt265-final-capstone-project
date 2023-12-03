3c31e6c09c85ea68a78556ca5dd7ebde24dae12d
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-12-03 14:23:05 -0700

    added images to the object array for the cards

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,21 +1,21 @@
 //dance program array
 export const danceObj = [
-  {img: ,
+  {img: 'images/programs/jazz-program-pexels.jpg',
   title: 'Jazz',
   cta: 'Learn More',
   url: },
 
-  {img: ,
+  {img: 'images/programs/contemporary-dance--program-pexels.jpg',
   title: 'Contemporary Dance',
   cta: 'Learn More',
   url: },
 
-  {img: ,
+  {img: 'images/programs/hip-hop-program-pexels.jpg',
   title: 'Hip Hop',
   cta: 'Learn More',
   url: },
 
-  {img: ,
+  {img: 'images/programs/west-african-program-pexels.jpg',
   title: 'West African',
   cta: 'Learn More',
   url: }
