45610707be9b9d4a447f344fcb8e6a3146098747
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-12-03 11:18:06 -0700

    added more objects to object array for cards

--- src/lib/data.js
+++ src/lib/data.js
@@ -3,5 +3,30 @@ export const danceObj = [
   {img: ,
   title: 'Jazz',
   cta: 'Learn More',
+  url: },
+
+  {img: ,
+  title: 'Contemporary Dance',
+  cta: 'Learn More',
+  url: },
+
+  {img: ,
+  title: 'Hip Hop',
+  cta: 'Learn More',
+  url: },
+
+  {img: ,
+  title: 'West African',
+  cta: 'Learn More',
+  url: }
+
+  {img: ,
+  title: 'Youth Classes',
+  cta: 'Learn More',
+  url: },
+
+  {img: ,
+  title: 'Jazz',
+  cta: 'Learn More',
   url: }
 ]
