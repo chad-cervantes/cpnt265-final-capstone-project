386f1b3395a1ef0afeea86f8438a8f3ce5a07d3b
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-12-03 16:10:08 -0700

    added content to the about +page.svelte

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -6,5 +6,21 @@
     <div class="h-max-[60%] z-10 bg-secondary-100 col-start-2 row-start-2 row-span-full text-slate-50 self-end p-4 overflow-hidden m-auto relative mb-[390px] ml-[-480px]">
       <h3 class="text-4xl pb-2 text-center font-bold">ABOUT ME</h3>
     </div>
-</div>
+  </div>
 </main>
+
+<h1 class="text-center text-[30px] font-bold p-8 mt-[-340px]">MEET VANESSA PADILLO</h1>
+
+<section class="grid grid-cols-2 w-[75%] m-auto">
+  <p class="text-left p-4 m-auto">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam, doloremque quod vel soluta delectus repellendus minus repudiandae pariatur dolorem molestiae. Esse quibusdam natus hic necessitatibus minima ipsa fuga sunt totam.</p>
+
+  <img src='images/about/about-photo-1.jpg' alt='studio choreography' class="w-[75%] h-[75%] m-auto">
+
+  <img src='images/about/about-photo-3.jpg' alt='studio choreography'  class="w-[75%] h-[75%] m-auto">
+
+  <p class="text-left p-4 m-auto">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam, doloremque quod vel soluta delectus repellendus minus repudiandae pariatur dolorem molestiae. Esse quibusdam natus hic necessitatibus minima ipsa fuga sunt totam.</p>
+
+  <p class="text-left p-4 m-auto">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam, doloremque quod vel soluta delectus repellendus minus repudiandae pariatur dolorem molestiae. Esse quibusdam natus hic necessitatibus minima ipsa fuga sunt totam.</p>
+
+  <img src='images/about/about-photo-5.jpg' alt='studio choreography'  class="w-[75%] h-[75%] m-auto">
+</section>
