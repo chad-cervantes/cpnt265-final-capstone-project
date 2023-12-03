0b56bfe9b0cea03a9d2a97dcabaa5177edb7d5e1
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-12-02 22:30:03 -0700

    made hero-section image with text in it

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,11 @@
-<script>
-  import PageHeader from "../lib/components/PageHeader.svelte";
-</script>
-
-<PageHeader />
-
-<slot />
+<main>
+  <!-- hero section -->
+  <div class="grid grid-cols-3 grid-rows-2">
+    <img src='images/gallery-photo-2.jpg' alt='studio choreography' class="col-span-full col-start-1 row-start-1 row-span-full w-screen h-[600px]">
+  
+    <div class="h-max-[60%] z-10 bg-secondary-100 col-start-2 row-start-2 row-span-full text-slate-50 self-end p-4 overflow-hidden m-auto relative mt-[-270px]">
+      <h3 class="text-4xl pb-2 text-center">COMMON GROUND DANCE</h3>
+      <p class="text-center text-xl">Move to the Rhythm, Dance to Your Heartbeat!</p>
+    </div>
+</div>
+</main>
