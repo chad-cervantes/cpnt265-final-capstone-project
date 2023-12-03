207397dc85b6b7acc13092e5dd2a354f6ea65676
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-12-03 14:32:48 -0700

    added Youth and Adult classes images in object array for cards

--- src/lib/data.js
+++ src/lib/data.js
@@ -3,30 +3,30 @@ export const danceObj = [
   {img: 'images/programs/jazz-program-pexels.jpg',
   title: 'Jazz',
   cta: 'Learn More',
-  url: },
+  url: ''},
 
   {img: 'images/programs/contemporary-dance--program-pexels.jpg',
   title: 'Contemporary Dance',
   cta: 'Learn More',
-  url: },
+  url: ''},
 
   {img: 'images/programs/hip-hop-program-pexels.jpg',
   title: 'Hip Hop',
   cta: 'Learn More',
-  url: },
+  url: ''},
 
   {img: 'images/programs/west-african-program-pexels.jpg',
   title: 'West African',
   cta: 'Learn More',
-  url: }
+  url: ''}
 
-  {img: ,
+  {img: 'images/programs/youth-classes-pexels.jpg',
   title: 'Youth Classes',
   cta: 'Learn More',
-  url: },
+  url: ''},
 
-  {img: ,
-  title: 'Jazz',
+  {img: 'images/programs/adult-classes-pexels.jpg',
+  title: 'Adult Classes',
   cta: 'Learn More',
-  url: }
+  url: ''}
 ]
