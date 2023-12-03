89120add5f4ee2bdd5994a4f1a6f6eb97f6fa5f8
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-12-02 23:12:02 -0700

    made hero-section in programs +page.svelte

---
+++ src/routes/programs/+page.svelte
@@ -0,0 +1,11 @@
+<main>
+  <!-- hero section -->
+  <div class="grid grid-cols-3 grid-rows-2">
+    <img src='images/programs-hero-section-image.jpg' alt='studio choreography' class="col-span-full col-start-1 row-start-1 row-span-full w-screen h-[600px]">
+  
+    <div class="h-max-[60%] z-10 bg-secondary-100 col-start-2 row-start-2 row-span-full text-slate-50 self-end p-4 overflow-hidden m-auto relative mt-[-270px]">
+      <h3 class="text-4xl pb-2 text-center font-bold">START YOUR DANCE JOURNEY</h3>
+      <p class="text-center text-xl">Join the dance revolution to where every step is a celebration of life!</p>
+    </div>
+</div>
+</main>
