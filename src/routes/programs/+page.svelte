6135c778d7e7e698d850b78c962b1d1d67a6b3d3
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-12-03 11:19:37 -0700

    added programs folder in images folder for programs-related images

--- src/routes/programs/+page.svelte
+++ src/routes/programs/+page.svelte
@@ -1,7 +1,7 @@
 <main>
   <!-- hero section -->
   <div class="grid grid-cols-3 grid-rows-2">
-    <img src='images/programs-hero-section-image.jpg' alt='studio choreography' class="col-span-full col-start-1 row-start-1 row-span-full w-screen h-[600px]">
+    <img src='images/programs/programs-hero-section-image.jpg' alt='studio choreography' class="col-span-full col-start-1 row-start-1 row-span-full w-screen h-[600px]">
   
     <div class="h-max-[60%] z-10 bg-secondary-100 col-start-2 row-start-2 row-span-full text-slate-50 self-end p-4 overflow-hidden m-auto relative mt-[-270px]">
       <h3 class="text-4xl pb-2 text-center font-bold">START YOUR DANCE JOURNEY</h3>
