3bd638ff0b7cedf23894025c5a3ea9669eea1080
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-12-03 15:02:07 -0700

    made h3 text bigger for cards

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -8,7 +8,7 @@
 <article class="lg:w-[550px] bg-red-300 bg-contain  bg-cover text-xl text-center border-solid scale-75 m-auto rounded-[5%] p-8 hover:bg-orange-400 active:bg-teal-500 transition ease-linear duration-150">
   <a href={url}>
     <img src={img} class="w-[500px] h-[350px] m-auto rounded-[5%]" alt="dance class">
-    <h3>{title}</h3>
+    <h3 class="p-4 text-6xl">{title}</h3>
     <p>{cta}</p>
   </a>
 </article>
