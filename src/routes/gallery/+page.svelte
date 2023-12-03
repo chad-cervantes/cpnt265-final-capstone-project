9b9b363cdcc053b3eba3ad1ab779569c249e0e8a
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-12-03 10:20:23 -0700

    added gallery in src route for images to render; added extra photos; changed title of images

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,36 +1,21 @@
-<!-- <div class="m-auto w-[75%] h-[50%] p-4 gap-4 lg:grid grid-cols-4">
-  <imgsrc="/images/gallery-photo-1.jpg" alt="dance photo 1">
-  <img src="/images/gallery-photo-2.jpg" alt="dance photo 2">
-  <img src="/images/gallery-photo-3.jpg" alt="dance photo 3">
-  <img src="/images/gallery-photo-4.jpg" alt="dance photo 4">
-  <img src="/images/gallery-photo-5.jpg" alt="dance photo 5">
-  <img src="/images/gallery-photo-6.jpg" alt="dance photo 6">
-  <img src="/images/gallery-photo-7.jpg" alt="dance photo 7">
-  <img src="/images/gallery-photo-8.jpg" alt="dance photo 8">
-  <img src="/images/gallery-photo-9.jpg" alt="dance photo 9">
-  <img src="/images/gallery-photo-10.jpg" alt="dance photo 10">
-  <img src="/images/gallery-photo-11.jpg" alt="dance photo 11">
-  <img src="/images/gallery-photo-12.png" alt="dance photo 12">
-</div> -->
-
 <script context="module" lang="ts">
   import ImageCarousel from "../../lib/components/ImageCarousel.svelte";
 
   export const prerender = true
 
   let images = [
-    {title: 'Image 1', src:'/images/gallery-photo-1.jpg'},
-    {title: 'Image 1', src:'/images/gallery-photo-1.jpg'},
-    {title: 'Image 1', src:'/images/gallery-photo-1.jpg'},
-    {title: 'Image 1', src:'/images/gallery-photo-1.jpg'},
-    {title: 'Image 1', src:'/images/gallery-photo-1.jpg'},
-    {title: 'Image 1', src:'/images/gallery-photo-1.jpg'},
-    {title: 'Image 1', src:'/images/gallery-photo-1.jpg'},
-    {title: 'Image 1', src:'/images/gallery-photo-1.jpg'},
-    {title: 'Image 1', src:'/images/gallery-photo-1.jpg'},
-    {title: 'Image 1', src:'/images/gallery-photo-1.jpg'},
-    {title: 'Image 1', src:'/images/gallery-photo-1.jpg'},
-    {title: 'Image 1', src:'/images/gallery-photo-1.jpg'},
+    {title: 'Image 1', src:'/images/gallery/gallery-photo-1.jpg'},
+    {title: 'Image 2', src:'/images/gallery/gallery-photo-2.jpg'},
+    {title: 'Image 3', src:'/images/gallery/gallery-photo-3.jpg'},
+    {title: 'Image 4', src:'/images/gallery/gallery-photo-4.jpg'},
+    {title: 'Image 5', src:'/images/gallery/gallery-photo-5.jpg'},
+    {title: 'Image 6', src:'/images/gallery/gallery-photo-6.jpg'},
+    {title: 'Image 7', src:'/images/gallery/gallery-photo-7.jpg'},
+    {title: 'Image 8', src:'/images/gallery/gallery-photo-8.jpg'},
+    {title: 'Image 9', src:'/images/gallery/gallery-photo-9.jpg'},
+    {title: 'Image 10', src:'/images/gallery/gallery-photo-10.jpg'},
+    {title: 'Image 11', src:'/images/gallery/gallery-photo-11.jpg'},
+    {title: 'Image 12', src:'/images/gallery/gallery-photo-12.png'},
   ]
 </script>
 
