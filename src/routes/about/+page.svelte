4943c35284b474319cc23693216d79a4a4ff4e05
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-12-03 10:13:10 -0700

    added gallery in the img src routefor image to render

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,7 +1,7 @@
 <main>
   <!-- hero section -->
   <div class="grid grid-cols-3 grid-rows-2">
-    <img src='images/gallery-photo-3.jpg' alt='studio choreography' class="col-span-full col-start-1 row-start-1 row-span-full w-screen h-[600px]">
+    <img src='images/gallery/gallery-photo-3.jpg' alt='studio choreography' class="col-span-full col-start-1 row-start-1 row-span-full w-screen h-[600px]">
   
     <div class="h-max-[60%] z-10 bg-secondary-100 col-start-2 row-start-2 row-span-full text-slate-50 self-end p-4 overflow-hidden m-auto relative mb-[390px] ml-[-480px]">
       <h3 class="text-4xl pb-2 text-center font-bold">ABOUT ME</h3>
