b684466f8a208c9d0a68aac02e11c53bf2ebdd51
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-12-04 10:50:20 -0700

    rendered the calendar in the schedule +page.svelte

---
+++ src/routes/schedule/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { Calendar } from '@fullcalendar/core';
+  import timeGridPlugin from '@fullcalendar/timegrid';
+  import dayGridPlugin from '@fullcalendar/daygrid';
+  import listPlugin from '@fullcalendar/list';
+  import interactionPlugin from '@fullcalendar/interaction';
+  import multiMonthPlugin from '@fullcalendar/multimonth';
+  import { onMount } from 'svelte';
+  
+  onMount(() => {
+  const calendarEl = document.getElementById('calendar');
+
+  let calendar = new Calendar(calendarEl, {
+  plugins: [ dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin, multiMonthPlugin ],
+  initialView: 'dayGridMonth',
+  headerToolbar: {
+    left: 'prev,next today',
+    center: 'title',
+    right: 'dayGridMonth,timeGridWeek,listWeek'
+  }
+  });
+
+  calendar.render();
+  });
+
+</script>
+
+<div id="calendar"></div>
