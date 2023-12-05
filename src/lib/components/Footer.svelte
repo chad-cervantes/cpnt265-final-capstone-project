2a8bfb76cbd6a7b813468cb09c6c1c849afd4947
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-12-04 21:32:37 -0700

    edited Footer.svelte; moved the items around in proper place and made hover better

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -12,15 +12,15 @@
 </script>
 
 <!--Footer Nav Links decoration-->
-<header class="bg-black text-white text-md flex justify-center w-[100%] h-[100%] p-4">
+<header class="bg-black text-white text-md flex justify-evenly w-[100%] h-[100%] p-4">
   <div>
-    <img src="images/common-ground-dance-logo.png" class="block w-[100px] h-[75px] p-4 mb-5 bg-cover" alt="Common Ground Dance logo">
+    <img src="images/common-ground-dance-logo.png" class="block w-[100px] h-[75px] p-4 mb-5 mt-[20px] bg-cover m-auto" alt="Common Ground Dance logo">
     <p class="mt-[50px] text-center">&copy 2023 Common Ground Dance</p>
   </div>
   <nav>
-    <ul class="lg:grid grid-cols-3 text-lg text-center space-x-12 gap-4 p-4">
+    <ul class="lg:grid grid-cols-3 text-lg text-center space-x-12 gap-x-52 gap-y-4 p-4">
       {#each navLinks as {name, href}}
-        <div class="hover:text-cyan-200 active:text-emerald-500">
+        <div class="hover:bg-purple-600 rounded-[20px] transition duration-150 ease-linear active:bg-rose-600 px-2 rounded-[10px]">
           <li><a {href}>{name}</a></li>
         </div>
       {/each}  
