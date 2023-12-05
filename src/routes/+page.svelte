811685c68697ccc877499651ab219970c65911b2
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-12-04 18:14:45 -0700

    added an autoplay video to main +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,12 +4,17 @@
 
 <main>
   <!-- hero section -->
-  <div class="grid grid-cols-3 grid-rows-2">
-    <img src='images/gallery/gallery-photo-2.jpg' alt='studio choreography' class="col-span-full col-start-1 row-start-1 row-span-full w-screen h-[600px]">
+  <div>
+    <!-- <img src='images/gallery/gallery-photo-2.jpg' alt='studio choreography' class="col-span-full col-start-1 row-start-1 row-span-full w-screen h-[600px]"> -->
+    <section class="h-[50vh] w-[100%]">
+      <video autoplay loop muted plays-inline>
+        <source src='/videos/pexels-cottonbro-7975419 (720p).mp4' type='video/mp4'>
+      </video>
+    </section>
   
     <div class="h-max-[60%] z-10 bg-secondary-100 col-start-2 row-start-2 row-span-full text-slate-50 self-end p-4 overflow-hidden m-auto relative mt-[-270px]">
       <h3 class="text-4xl pb-2 text-center font-bold">COMMON GROUND DANCE</h3>
       <p class="text-center text-xl">Move to the Rhythm, Dance to Your Heartbeat!</p>
     </div>
-</div>
+  </div>
 </main>
