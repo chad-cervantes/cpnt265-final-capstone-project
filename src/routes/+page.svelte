4841b75527aae9efe96d5c3be1611fe845591e98
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-12-04 19:37:38 -0700

    re-sized hero-section-video

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,15 +4,15 @@
 
 <main>
   <!-- hero section -->
-  <div>
+  <div class="min-h-screen">
     <!-- <img src='images/gallery/gallery-photo-2.jpg' alt='studio choreography' class="col-span-full col-start-1 row-start-1 row-span-full w-screen h-[600px]"> -->
-    <section class="h-[50vh] w-[100%]">
-      <video autoplay loop muted plays-inline>
+    <section class="relative flex h-[100vh] shrink-0 flex-col items-center justify-center">
+      <video autoplay loop muted plays-inline disablepictureinpicture class="absolute w-full h-full object-cover -z-10">
         <source src='/videos/pexels-cottonbro-7975419 (720p).mp4' type='video/mp4'>
       </video>
     </section>
   
-    <div class="h-max-[60%] z-10 bg-secondary-100 col-start-2 row-start-2 row-span-full text-slate-50 self-end p-4 overflow-hidden m-auto relative mt-[-270px]">
+    <div class="h-max-[60%] z-10 bg-secondary-100 col-start-2 row-start-2 row-span-full text-slate-50 self-end p-4 overflow-hidden m-auto relative mt-[-670px]">
       <h3 class="text-4xl pb-2 text-center font-bold">COMMON GROUND DANCE</h3>
       <p class="text-center text-xl">Move to the Rhythm, Dance to Your Heartbeat!</p>
     </div>
