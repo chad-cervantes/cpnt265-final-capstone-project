5ef6e2dd7a38bafc89ca535065341e22855a3320
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-12-05 15:31:46 -0700

    added contact info from phone, email, to social media

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+  import Icon from '@iconify/svelte';
+</script>
+
 <main class="min-h-[110vh]">
   <!-- hero section -->
   <div class="grid grid-cols-3 grid-rows-2 ">
@@ -12,16 +16,48 @@
 
 </main>
 
-  <section class="flex w-full min-h-screen justify-center items-center mt-[-340px]">
-    <div class="flex flex-col space-y-6 bg-pink-400 w-full max-w-4xl p-8 rounded-xl shadow-lg text-white">
-
-    <div>
+<section class="flex w-full min-h-screen justify-center items-center mt-[-440px]">
+  <div class="flex flex-col space-y-6 bg-pink-400 w-full max-w-4xl p-8 rounded-xl shadow-lg text-white">
+    
+    <div class="flex flex-col space-y-8 justify-between">
       <div>
         <h1 class="font-bold text-4xl tracking-wide">Contact Me</h1>
-        <p class="pt-2 text-sm">Whether you have general or specific questions, inquire here!</p>
+        <p class="pt-2 text-md">Whether you have general or specific questions, inquire here!</p>
       </div>
-      <div></div>
-      <div></div>
-    </div>
-  </div>
-  </section>
+      
+      <div class="flex gap-4">
+        <Icon icon="ph:phone-light" width="30" />
+        <span>Toll Free: 1-877-5-DANCE-5</span>
+      </div>
+      
+      <div class="flex gap-4">
+        <Icon icon="ic:outline-email" width="30" />
+        <span>commongrounddance@gmail.com</span>
+      </div>
+
+      <div class="flex gap-4">
+        <Icon icon="mdi:location" width="30" />
+        <span>Calgary, AB</span>
+      </div>
+
+      <div class="flex gap-4">
+        <a href="https://www.facebook.com/groups/195302860182">
+          <Icon icon="ic:baseline-facebook" width="30"  class="hover:opacity-50 transition duration-150 ease-in"/>
+        </a>
+
+        <a href="https://www.youtube.com/@CommonGroundDance">
+          <Icon icon="mingcute:youtube-line" width="30" class="hover:opacity-50 transition duration-150 ease-in"/>
+        </a>
+
+        <a href="https://www.instagram.com/vavooooom/?hl=en">
+          <Icon icon="mdi:instagram" width="30" class="hover:opacity-50 transition duration-150 ease-in"/>
+        </a>
+
+        <a href="https://www.linkedin.com/in/vanessa-padillo-cpa-cma-743bb635/" class="hover:opacity-50 transition duration-150 ease-in">
+          <Icon icon="mdi:linkedin" width="30"/>
+        </a>
+
+      </div>
+   </div>
+ <div>
+</section>
