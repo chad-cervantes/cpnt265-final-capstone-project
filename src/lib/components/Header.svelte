06c161c9446ff2747f3757b22e59ca8c680d2851
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-12-04 17:52:12 -0700

    styled the <Header.svelte />

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,23 +1,23 @@
 <script>
   //Primary Navbar
    const navLinks = [
-    {name: 'Home', href: '/'},
-    {name: 'Programs', href: '/programs'},
-    {name: 'Gallery', href: '/gallery'},
-    {name: 'Schedule', href: '/schedule'},
-    {name: 'About', href: '/about'},
-    {name: 'Contact', href: '/contact'}
+    {name: 'HOME', href: '/'},
+    {name: 'PROGRAMS', href: '/programs'},
+    {name: 'GALLERY', href: '/gallery'},
+    {name: 'SCHEDULE', href: '/schedule'},
+    {name: 'ABOUT', href: '/about'},
+    {name: 'CONTACT', href: '/contact'}
   ];
 
 </script>
 
 <!--Primary Nav Links decoration-->
-<header class="bg-violet-600 text-white text-xl flex justify-center w-[auto] h-[100px] p-4">
+<header class="bg-violet-400 text-white text-xl flex justify-center w-[auto] h-[100px] p-4">
   <div><img src="images/common-ground-dance-logo.png" class="block w-[100px] h-[75px] p-4 mb-5 bg-cover" alt="Common Ground Dance logo"></div>
   <nav>
-    <ul class="lg:flex text-2xl space-x-12 gap-4 p-4">
+    <ul class="lg:flex text-xl space-x-12 gap-2 p-4">
       {#each navLinks as {name, href}}
-        <div class="hover:text-cyan-200 active:text-emerald-500">
+        <div class="hover:bg-purple-600 rounded-[20px] active:bg-rose-600 px-2 rounded-[20px] ">
           <li><a {href}>{name}</a></li>
         </div>
       {/each}  
