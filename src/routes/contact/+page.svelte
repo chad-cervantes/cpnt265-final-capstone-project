b3c2cdafd515d63b59561b88117a6adf55cec3ed
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-12-05 14:52:28 -0700

    added content to the contact +page.svelte

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,10 +1,27 @@
-<main>
+<main class="min-h-[110vh]">
   <!-- hero section -->
-  <div class="grid grid-cols-3 grid-rows-2">
+  <div class="grid grid-cols-3 grid-rows-2 ">
     <img src='images/contact/contact-image-1.jpg' alt='studio choreography' class="col-span-full col-start-1 row-start-1 row-span-full w-screen h-[600px]">
   
     <div class="h-max-[60%] z-10 bg-secondary-100 col-start-2 row-start-2 row-span-full text-slate-50 self-end p-4 overflow-hidden m-auto relative mb-[390px] ml-[-480px]">
-      <h3 class="text-4xl pb-2 text-center font-bold">CONTACT ME</h3>
+      <h3 class="text-4xl pb-2 text-center font-bold">CONTACT INFO</h3>
     </div>
   </div>
+<!-- 
+  <h1 class="text-[30px] text-center font-bold p-8 mt-[-340px]">ANY QUESTIONS?</h1> -->
+
 </main>
+
+  <section class="flex w-full min-h-screen justify-center items-center mt-[-340px]">
+    <div class="flex flex-col space-y-6 bg-pink-400 w-full max-w-4xl p-8 rounded-xl shadow-lg text-white">
+
+    <div>
+      <div>
+        <h1 class="font-bold text-4xl tracking-wide">Contact Me</h1>
+        <p class="pt-2 text-sm">Whether you have general or specific questions, inquire here!</p>
+      </div>
+      <div></div>
+      <div></div>
+    </div>
+  </div>
+  </section>
