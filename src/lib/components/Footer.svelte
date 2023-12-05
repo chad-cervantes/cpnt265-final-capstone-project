a39c6704abc632f35e4bc72cb5189c74c71f3fd3
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-12-05 09:06:08 -0700

    updated copyright in Footer.svelte

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -15,7 +15,7 @@
 <header class="bg-black text-white text-md flex justify-evenly w-[100%] h-[100%] p-4">
   <div>
     <img src="images/common-ground-dance-logo.png" class="block w-[100px] h-[75px] p-4 mb-5 mt-[20px] bg-cover m-auto" alt="Common Ground Dance logo">
-    <p class="mt-[50px] text-center">&copy 2023 Common Ground Dance</p>
+    <p class="mt-[50px] text-center">&copy 2023 Common Ground Dance Inc. All Rights Reserved.</p>
   </div>
   <nav>
     <ul class="lg:grid grid-cols-3 text-lg text-center space-x-12 gap-x-52 gap-y-4 p-4">
