78dbd27b3f3d979a9f8713208ad19b7fcdffb924
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-12-05 14:06:29 -0700

    added more images to the carousel

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -16,10 +16,19 @@
     {title: 'Image 10', src:'/images/gallery/gallery-photo-10.jpg'},
     {title: 'Image 11', src:'/images/gallery/gallery-photo-11.jpg'},
     {title: 'Image 12', src:'/images/gallery/gallery-photo-12.png'},
+    {title: 'Portrait Image 1', src:'/images/gallery/portrait-photo-1.jpg'},
+    {title: 'Portrait Image 2', src:'/images/gallery/protrait-photo-2.jpg'},
+    {title: 'Portrait Image 3', src:'/images/gallery/portrait-photo-3.jpg'},
+    {title: 'Portrait Image 4', src:'/images/gallery/portrait-photo-4.jpg'},
+    {title: 'Portrait Image 5', src:'/images/gallery/portrait-photo-5.jpg'},
+    {title: 'Portrait Image 6', src:'/images/gallery/protrait-photo-6.jpg'},
+    {title: 'Portrait Image 7', src:'/images/gallery/protrait-photo-7.jpg'},
+    {title: 'Portrait Image 8', src:'/images/gallery/protrait-photo-8.jpg'},
   ]
+
 </script>
 
-<h1 class="p-4 text-center text-[50px] font-bold">PERFORMANCE</h1>
+<h1 class="p-4 text-center text-[50px] font-bold">PERFORMANCE & PORTRAITS</h1>
 
 <section class="flex flex-col items-center">
     <ImageCarousel {images} />
