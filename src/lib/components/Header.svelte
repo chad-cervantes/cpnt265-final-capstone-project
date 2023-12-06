bcb026df28687bf660723b6e90f7f4f8c231e5bb
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-12-06 10:15:09 -0700

    indented code

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -12,36 +12,39 @@
   import Icon from '@iconify/svelte';
 </script>
 
+
 <!--Primary Nav Links decoration-->
-<header class="bg-violet-400 text-white text-xl flex justify-evenly w-[auto] h-[100px] p-4">
-  <div><img src="images/common-ground-dance-logo.png" class="block w-[100px] h-[75px] p-4 mb-5 bg-cover" alt="Common Ground Dance logo"></div>
-  <nav>
-    <ul class="lg:flex text-xl space-x-12 gap-2 p-4 mt-2">
-      {#each navLinks as {name, href}}
-        <div class="hover:bg-purple-600 rounded-[20px] transition duration-150 ease-linear active:bg-rose-600 px-2 rounded-[20px]">
-          <li><a {href}>{name}</a></li>
-        </div>
-      {/each}  
 
-      <!--Social Media-->
-      <div class="flex gap-4">
-        <a href="https://www.facebook.com/groups/195302860182">
-          <Icon icon="ic:baseline-facebook" width="30"  class="hover:opacity-50 transition duration-150 ease-in"/>
-        </a>
+  <header class="bg-violet-400 text-white text-xl flex justify-evenly w-[auto] h-[100px] p-4">
+    <div><img src="images/common-ground-dance-logo.png" class="block w-[100px] h-[75px] p-4 mb-5 bg-cover" alt="Common Ground Dance logo"></div>
+    
+      <nav>
+        <ul class="lg:flex text-xl space-x-12 gap-2 p-4 mt-2">
+          {#each navLinks as {name, href}}
+            <div class="hover:bg-purple-600 rounded-[20px] transition duration-150 ease-linear active:bg-rose-600 px-2 rounded-[20px]">
+              <li><a {href}>{name}</a></li>
+            </div>
+          {/each}  
 
-        <a href="https://www.youtube.com/@CommonGroundDance">
-          <Icon icon="mingcute:youtube-line" width="30" class="hover:opacity-50 transition duration-150 ease-in"/>
-        </a>
+          <!--Social Media-->
+          <div class="flex gap-4">
+            <a href="https://www.facebook.com/groups/195302860182">
+              <Icon icon="ic:baseline-facebook" width="30"  class="hover:opacity-50 transition duration-150 ease-in"/>
+            </a>
 
-        <a href="https://www.instagram.com/vavooooom/?hl=en">
-          <Icon icon="mdi:instagram" width="30" class="hover:opacity-50 transition duration-150 ease-in"/>
-        </a>
+            <a href="https://www.youtube.com/@CommonGroundDance">
+              <Icon icon="mingcute:youtube-line" width="30" class="hover:opacity-50 transition duration-150 ease-in"/>
+            </a>
 
-        <a href="https://www.linkedin.com/in/vanessa-padillo-cpa-cma-743bb635/" class="hover:opacity-50 transition duration-150 ease-in">
-          <Icon icon="mdi:linkedin" width="30"/>
-        </a>
-      </div>
+            <a href="https://www.instagram.com/vavooooom/?hl=en">
+              <Icon icon="mdi:instagram" width="30" class="hover:opacity-50 transition duration-150 ease-in"/>
+            </a>
 
-    </ul>
-  </nav>
-</header>
+            <a href="https://www.linkedin.com/in/vanessa-padillo-cpa-cma-743bb635/" class="hover:opacity-50 transition duration-150 ease-in">
+              <Icon icon="mdi:linkedin" width="30"/>
+            </a>
+          </div>
+
+        </ul>
+      </nav>
+  </header>
