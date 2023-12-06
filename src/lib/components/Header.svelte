00656406e356f9fc83d31c60430a09fc4ec6d12f
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-12-06 12:32:03 -0700

    fixed menu toggle nav links since they were showing up on the desktop

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -24,9 +24,9 @@
 </script>
 
 <!--Primary Nav Links decoration-->
-  <header class="bg-violet-400 text-white text-xl flex justify-evenly w-[auto] h-[100px] p-4">
+  <header class="bg-violet-400 text-white text-xl flex justify-evenly w-[auto] h-[auto] p-4">
     <div>
-      <img src="images/common-ground-dance-logo.png" class="block w-[100px] h-[75px] p-4 mb-5 bg-cover" alt="Common Ground Dance logo">
+      <img src="images/common-ground-dance-logo.png" class="block w-[100px] h-[75px] p-4 mb-[5px] bg-cover" alt="Common Ground Dance logo">
     </div>
    
     <div class="lg:hidden block">
@@ -36,15 +36,15 @@
  
     {#if open}
       <nav transition:fly={{ y: -200, duration:400 }}>
-        <ul class="lg:flex text-xl space-x-12 gap-2 p-4 mt-2 ">
+        <ul class="lg:flex text-xl space-x-12 gap-2 p-4 mt-2">
           {#each navLinks as {name, href}}
-            <div class="hover:bg-purple-600 rounded-[20px] transition duration-150 ease-linear active:bg-rose-600 px-2 rounded-[20px]">
+            <div class="hover:bg-purple-600 rounded-[20px] transition duration-150 ease-linear active:bg-rose-600 px-2 rounded-[20px] lg:hidden">
               <li><a {href}>{name}</a></li>
             </div>
           {/each}  
 
                <!--Social Media-->
-          <div class="flex gap-4">
+          <div class="flex gap-4 lg:hidden">
             <a href="https://www.facebook.com/groups/195302860182">
               <Icon icon="ic:baseline-facebook" width="30"  class="hover:opacity-50 transition duration-150 ease-in"/>
             </a>
