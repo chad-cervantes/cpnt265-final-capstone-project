882820acac56857eefe221dab7850214b787e639
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-12-05 21:45:22 -0700

    made primary links center with the logo

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -15,7 +15,7 @@
 <header class="bg-violet-400 text-white text-xl flex justify-center w-[auto] h-[100px] p-4">
   <div><img src="images/common-ground-dance-logo.png" class="block w-[100px] h-[75px] p-4 mb-5 bg-cover" alt="Common Ground Dance logo"></div>
   <nav>
-    <ul class="lg:flex text-xl space-x-12 gap-2 p-4">
+    <ul class="lg:flex text-xl space-x-12 gap-2 p-4 mt-2">
       {#each navLinks as {name, href}}
         <div class="hover:bg-purple-600 rounded-[20px] transition duration-150 ease-linear active:bg-rose-600 px-2 rounded-[20px]">
           <li><a {href}>{name}</a></li>
