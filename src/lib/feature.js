d95696b899ccb0056a2ad983718cad2eab82b928
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-12-06 15:04:24 -0700

    added feature.js as the object array for the Feature Cards

---
+++ src/lib/feature.js
@@ -0,0 +1,11 @@
+export const featureObj = [
+  {img: '/images/home/home-photo-1.jpg',
+  title: 'CBC Calgary',
+  summary: 'This is an article from CBC Calgary about how I used my financial background to grow the arts community here in Calgary',
+  url: 'https://www.cbc.ca/amp/1.6464049'},
+
+  {img: '/images/home/home-photo-2.jpg',
+  title: 'Dance Resume',
+  summary: 'Here is a document of all of my previous and current dance experiences!',
+  url: '/documents/dance-resume.pdf'}
+]
