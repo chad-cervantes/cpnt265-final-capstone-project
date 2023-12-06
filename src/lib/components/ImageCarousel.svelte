6979af101a3c79e6e396bce66249e4e864a576a8
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-12-06 08:27:04 -0700

    able to click the title of the image and re-direct you to that image

--- src/lib/components/ImageCarousel.svelte
+++ src/lib/components/ImageCarousel.svelte
@@ -4,12 +4,24 @@
     src: string
   }
   export let images: ImageType[]
+  export let name = 'carousel'
+  export const getItemId = (gallery: number) => `carousel-item-${gallery}`
 </script>
 
 <ul class="flex overflow-x-auto gap-6 snap-x snap-mandatory before:shrink-0 before:w-[30vw] after:shrink-0 after:w-[30vw] p-4">
-  {#each images as {title,src}}
-    <li class="shrink-0 snap-center">
+  {#each images as {title,src}, gallery}
+    <li id={getItemId(gallery)} class="shrink-0 snap-center">
       <img {title} {src} alt={title} class="w-[300px] h-[500px]"/>
     </li>
   {/each}
 </ul>
+
+<nav>
+  <ul>
+    {#each images as {title}, gallery}
+      <li class="shrink-0 snap-center">
+        <a href={`#${getItemId(gallery)}`}>{title}</a>
+      </li>
+    {/each}
+  </ul>
+</nav>
