3ad83c9ec865bd4982b2c64e4928df66e4f3b0fb
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-12-06 15:06:02 -0700

    made the cards and text a different color, purple and blue

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -5,10 +5,10 @@
   export let url;
 </script>
 
-<article class="lg:w-[550px] bg-red-300 bg-contain  bg-cover text-xl text-center border-solid scale-75 m-auto rounded-[5%] p-8 hover:bg-orange-400 active:bg-teal-500 transition ease-linear duration-150">
+<article class="lg:w-[550px] bg-purple-300 bg-contain  bg-cover text-xl text-center border-solid scale-75 m-auto rounded-[5%] p-8 hover:bg-pink-400 active:bg-yellow-500 transition ease-linear duration-150">
   <a href={url}>
     <img src={img} class="w-[500px] h-[350px] m-auto rounded-[5%]" alt="dance class">
-    <h3 class="p-4 text-6xl">{title}</h3>
-    <p>{cta}</p>
+    <h3 class="p-4 text-6xl text-blue-600 font-bold">{title}</h3>
+    <p class="p-4 text-3xl">{cta}</p>
   </a>
 </article>
