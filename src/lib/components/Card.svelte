cffcb23a4fdceba78d4e4e39cb85bfa13ae87ac0
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-12-07 11:29:56 -0700

    made text of program cards more responsive

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -8,7 +8,7 @@
 <article class="lg:w-[550px] bg-purple-300 bg-contain  bg-cover text-xl text-center border-solid scale-75 m-auto rounded-[5%] p-8 hover:bg-pink-400 active:bg-yellow-500 transition ease-linear duration-150">
   <a href={url}>
     <img src={img} class="w-[500px] h-[350px] m-auto rounded-[5%]" alt="dance class">
-    <h3 class="p-4 lg:p-4 text-6xl text-blue-600 text-center font-bold">{title}</h3>
+    <h3 class="p-4 text-5xl lg:p-4 text-blue-600 text-center font-extrabold">{title}</h3>
     <p class="p-4 lg:p-4 text-3xl">{cta}</p>
   </a>
 </article>
