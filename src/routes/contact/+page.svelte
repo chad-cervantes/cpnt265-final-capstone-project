ca2d8c60f2395368957518f6ab6862437eb55cf5
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-12-09 17:07:05 -0700

    ensured that hero-section title were aligned the same

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -7,7 +7,7 @@
   <div class="grid grid-cols-3 grid-rows-2 ">
     <img src='images/gallery/gallery-photo-2.jpg' alt='studio choreography' class="col-span-full col-start-1 row-start-1 row-span-full w-screen h-[600px] m-auto">
   
-    <div class="h-max-[60%] z-10 bg-secondary-100 col-start-2 row-start-2 row-span-full text-slate-50 self-end p-4 overflow-hidden m-auto relative mb-[430px] mt-[-270px]">
+    <div class="h-max-[60%] z-10 bg-secondary-100 col-start-2 row-start-2 row-span-full text-slate-50 self-end p-4 overflow-hidden m-auto relative mt-[-270px]">
       <h3 class="text-4xl pb-2 text-center font-bold">CONTACT INFO</h3>
     </div>
   </div>
