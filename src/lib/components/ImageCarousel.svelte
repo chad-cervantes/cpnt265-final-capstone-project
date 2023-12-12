444508c6f7712c06e1055867fe958271de513eb7
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-12-11 21:18:50 -0700

    changed to w-[5vw] to get rid of extra white space in the image carousel

--- src/lib/components/ImageCarousel.svelte
+++ src/lib/components/ImageCarousel.svelte
@@ -7,7 +7,7 @@
   export const getItemId = (gallery: number) => `carousel-item-${gallery}`
 </script>
 
-<ul class="flex overflow-x-auto gap-6 snap-x snap-mandatory before:shrink-0 before:w-[30vw] after:shrink-0 after:w-[30vw] p-4 scroll-smooth ">
+<ul class="flex overflow-x-auto gap-6 snap-x snap-mandatory before:shrink-0 before:w-[5vw] after:shrink-0 after:w-[5vw] p-4 scroll-smooth ">
   {#each images as {title,src}, gallery}
     <li id={getItemId(gallery)} class="shrink-0 snap-center">
       <img {title} {src} alt={title} class="w-[300px] h-[500px]"/>
