cccd367886cf48160f49d9c03a5135256e154621
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2024-05-11 23:47:55 -0600

    changed directory path of favicon.ico

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,11 @@
   import "../app.css";
   import Header from "../lib/components/Header.svelte";
   import Footer from "../lib/components/Footer.svelte";
+  import { onMount } from 'svelte';
+
+onMount(() => {
+    document.querySelector('link[rel="icon"]').href = 'static/favicon.ico';
+});
 </script>
 <Header />
 <slot />
