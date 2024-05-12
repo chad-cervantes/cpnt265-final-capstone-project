d2385b589a6fef4fc8dea6cc3a9dc43e5c25269c
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2024-05-11 23:16:06 -0600

    allowed favicon to occur in vite.config.js and app.html

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	server: {
+    fs: {
+      allow: ['favicon.ico'] // Add the relative path to the directory containing favicon.ico
+    }
+  },
 	plugins: [sveltekit()]
 });
